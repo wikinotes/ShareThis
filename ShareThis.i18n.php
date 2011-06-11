61d6bd61077f3fe27291faffca032daf7d5b8984
Wesley <tahnok881@hotmail.com>
2011-06-11 16:44:22 -0400

    cleaned up code, fixed bug on special pages, added license and introduced internationalization support

--- ShareThis.i18n.php
+++ ShareThis.i18n.php
@@ -1,11 +1,8 @@
 <?php
-$aliases = array();
 $messages = array();
  
-$aliases['en'] = array(
-		       'ShareThis' => array( 'ShareThis', 'AnotherName' ),
-		       );
  
 $messages['en'] = array(
-			'sharethis' => 'My Extension',
+			'sharethis' => 'Share This',
+			'sharethis_link' => 'Tweet This Page'
 			);
--- ShareThis.php
+++ ShareThis.php
@@ -16,24 +16,26 @@ $wgExtensionCredits['specialpage'][] = array(
 	     );
 $dir = dirname(__FILE__) . '/';
  
-$wgAutoloadClasses['SpecialShareThis'] = $dir . 'SpecialShareThis.php'; # Location of the SpecialMyExtension class (Tell MediaWiki to load this file)
+
 $wgExtensionMessagesFiles['ShareThis'] = $dir . 'ShareThis.i18n.php'; # Location of a messages file (Tell MediaWiki to load this file)
 $wgSpecialPages['ShareThis'] = 'SpecialShareThis'; # Tell MediaWiki about the new special page and its class name
 
-$wgHooks['SkinTemplateBuildNavUrlsNav_urlsAfterPermalink'][] = 'workDamnIt';
-$wgHooks['SkinTemplateToolboxEnd'][] = 'Test2';
+$wgHooks['SkinTemplateBuildNavUrlsNav_urlsAfterPermalink'][] = 'urlBuilder';
+$wgHooks['SkinTemplateToolboxEnd'][] = 'ShareToolbox';
 
-function Test2( &$monobook ) {
-#Uncomment this when we have multi-lang support
-#  wfLoadExtensionMessages('ShareThis'); 
-    ?><li id="Test"><a href="<?php echo htmlspecialchars($monobook->data['nav_urls']['ShareThis']['href'])?>" target="_blank">Tweet Page</a></li>
-  <?php
+function ShareToolbox( &$monobook) {
+
+  if( isset( $monobook->data['nav_urls']['ShareThis'])){
+    ?><li id="Test"><a href="<?php echo htmlspecialchars($monobook->data['nav_urls']['ShareThis']['href'])?>" target="_blank"><?php echo htmlspecialchars($monobook->data['nav_urls']['ShareThis']['text'])?></a></li>
+      <?php
+      }
   return true;
 }
 
-function workDamnIt( &$skintemplate, &$nav_urls, &$oldid, &$revid ) {
+function urlBuilder( &$skintemplate, &$nav_urls, &$oldid, &$revid ) {
 #uncomment this when we have multi-lang support
-#  wfLoadExtensionMessages( 'ShareThis' );
+  wfLoadExtensionMessages( 'ShareThis' );
+  
   global $wgTwitterUserName;
 
   $title = Title::newFromText(wfUrlEncode("{$skintemplate->thispage}"));
@@ -45,11 +47,10 @@ function workDamnIt( &$skintemplate, &$nav_urls, &$oldid, &$revid ) {
   }
   
   $nav_urls['ShareThis'] = array(
-				 'text' => "Tweet this!",
+				 'text' => wfMsg( 'sharethis_link'),
 				 'href' => $href
 				 );
+
   return true;
 } 
-
-
 ?>
